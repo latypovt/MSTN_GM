fdf03500ab1c82b96137dbc5c861a0a965e8c3a3
Tim Latypov <timur.latypov@mail.utoronto.ca>
2023-06-16 19:00:37 -0400

    added shuffle and testing set evaluation

--- model.py
+++ model.py
@@ -22,6 +22,7 @@ condition = np.array(condition)
 # drop unnecessary columns
 gm_data = ml_dataframe.drop(columns=['age', 'sex', 'diagnosis', 'duration_of_ms', 'duration_of_pain',
        'side_of_pain'])
+gm_data = gm_data[np.random.default_rng(seed=42).permutation(gm_data.columns.values)]
 
 # class for removing highly correlated features 
 class Remove_correlateds():
@@ -53,18 +54,30 @@ rc = Remove_correlateds()
 x = rc.fit(gm_data).to_numpy()
 y = condition
 
+feature_set = []
+mean_acc = []
 # test
 for nest_index, test_index in kf.split(x,y):
     x_nest, x_test = x[nest_index], x[test_index]
     y_nest, y_test = y[nest_index], y[test_index]
     kf2 = StratifiedKFold(n_splits=10, shuffle=True, random_state=42)
     split = kf2.split(x_nest, y_nest)
-    featureselector = SFS(model, k_features="parsimonious", forward=False, floating=False, scoring="accuracy", cv=list(split), n_jobs=18, verbose=1)
+    featureselector = SFS(model, k_features="parsimonious", forward=False, floating=False, scoring="accuracy", cv=list(split), n_jobs=18, verbose=0)
     featureselector.fit(x_nest, y_nest)
     print('Best accuracy score: %.4f' % featureselector.k_score_)
     print('Best subset (indices):', featureselector.k_feature_idx_)
     print('Number of features:', len(featureselector.k_feature_idx_))
-    print('Best subset (corresponding names):', list(featureselector.k_feature_names_))
+    feature_set.extend(list(featureselector.k_feature_idx_))
+    x_nest = featureselector.transform(x_nest)
+    x_test = featureselector.transform(x_test)
+    model = Pipeline([("scaler", StandardScaler()), ("svm", SVC(kernel="linear", C=0.1))])
+    model.fit(x_nest, y_nest)
+    y_pred = model.predict(x_test)
+    acc = accuracy_score(y_test, y_pred)
+    print("Accuracy: %.4f" % acc)
+    mean_acc.append(acc)
+feature_set = pd.DataFrame([feature_set])
+print("Mean test accuracy: %.4f" % np.mean(mean_acc))
 
 #TODO
 # 1. follow main() function structure - modify this script so that it can be called from main()
