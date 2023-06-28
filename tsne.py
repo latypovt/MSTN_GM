1d978ba55e20713068baffb0f2fab6267311c0e7
magpiehoard <abigail.wolfensohn@mail.mcgill.ca>
2023-06-28 14:47:34 -0400

    altered tsne

--- tsne.py
+++ tsne.py
@@ -24,13 +24,14 @@ def main():
     ml_dataframe['diagnosis'] = [1 if dx =="MS-TN" else 0 for dx in ml_dataframe['diagnosis']]
     ml_dataframe['sex'] = [1 if sex=="f" else 0 for sex in ml_dataframe["sex"]]
     ml_dataframe['duration_categories'] = ['<5' if duration <5 else '5-10' if duration < 10 else '10-15' if duration <15 else '15-20' if duration<20 else '>20' for duration in ml_dataframe['duration_of_ms']]
+    ml_dataframe['edss_categories'] = ['0-1.5' if edss <1.5 else '1.5-3' if edss < 3 else '3-4.5' if edss <4.5 else '4.5-6' if edss<6 else '6-7.5' if edss<=7.5 else 'MS-TN' for edss in ml_dataframe['edss']]
 
     # set condition
-    #condition = list(ml_dataframe["diagnosis"])
-    #condition = np.array(condition)
+    condition = list(ml_dataframe["diagnosis"])
+    condition = np.array(condition)
 
     # drop unnecessary columns
-    morphological_data = ml_dataframe.drop(columns=['id', 'age', 'sex', 'diagnosis', 'duration_of_ms', 'duration_categories', 'duration_of_pain', 'side_of_pain','edss'])
+    morphological_data = ml_dataframe.drop(columns=['id', 'age', 'sex', 'diagnosis', 'duration_of_ms', 'duration_categories', 'duration_of_pain', 'side_of_pain','edss', 'edss_categories'])
     morphological_data = morphological_data[np.random.default_rng(seed=42).permutation(morphological_data.columns.values)]
 
     # configure tsne model
@@ -39,12 +40,13 @@ def main():
     tsne_data = tsne.fit_transform(morphological_data)
 
     # create dataframe
-    tsne_data = np.vstack((tsne_data.T, ml_dataframe["edss"])).T
-#add edss, age, sex, duration of MS binned
-
+    tsne_data = np.vstack((tsne_data.T, ml_dataframe["diagnosis"])).T    
     tsne_df = pd.DataFrame(data=tsne_data, columns=("Dim_1", "Dim_2", "diagnosis"))
+    tsne_df["edss_categories"] = ml_dataframe["edss_categories"]
+    tsne_df["age"] = ml_dataframe["age"]
+    tsne_df["sex"] = ml_dataframe["sex"]
+    tsne_df["duration_categories"] = ml_dataframe["duration_categories"]
 
-    
     matplotlib.font_manager.findfont('humor sans')
 
     # plot
@@ -56,9 +58,16 @@ def main():
       plt.savefig('out/tsne.png', dpi=300)
 
     with plt.xkcd():
-       fig, ax = plt.subplots(2,2 , figsize=(10,10))
-        sns.scatterplot(ax=ax[0,0], data=tsne_df, x="Dim_1", y="Dim_2", hue='diagnosis', sizes = 5, alpha=0.8)
-        sns.scatterplot(ax=ax[0,1], data=tsne_df, x="Dim_1", y="Dim_2", hue='diagnosis', sizes = 5, alpha=0.8)
+      fig, ax = plt.subplots(2, 2, figsize=(15, 15))
+      sns.scatterplot(ax=ax[0, 0], data=tsne_df, x="Dim_1", y="Dim_2", hue='age', sizes=5, alpha=0.8)
+      ax[0, 0].legend(loc='upper right', title='age', fontsize='xx-small')
+      sns.scatterplot(ax=ax[0, 1], data=tsne_df, x="Dim_1", y="Dim_2", hue='sex', sizes=5, alpha=0.8)
+      ax[0, 1].legend(loc='upper right', title='sex', fontsize='xx-small')
+      sns.scatterplot(ax=ax[1, 0], data=tsne_df, x="Dim_1", y="Dim_2", hue='duration_categories', sizes=5, alpha=0.8)
+      ax[1, 0].legend(loc='upper right', title='duration', fontsize='xx-small')
+      sns.scatterplot(ax=ax[1, 1], data=tsne_df, x="Dim_1", y="Dim_2", hue='edss_categories', sizes=5, alpha=0.8)
+      ax[1, 1].legend(loc='upper right', title='edss', fontsize='xx-small')
+      plt.savefig('out/tsne_subplots.png', dpi=300)
 
 
  
